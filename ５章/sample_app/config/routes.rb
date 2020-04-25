505fc45322f23c52c4bbf6d97ced190aec3ca0e8
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-04-25 19:21:48 +0900

    [finish]chap.5

---
+++ ５章/sample_app/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get '/signup', to: 'users#new', as: 'users_new'
+  get 'static_pages/help', to: 'static_pages#help'
+  get 'static_pages/about', to: 'static_pages#about'
+  get 'static_pages/contact', to: 'static_pages#contact'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+end
